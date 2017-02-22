166b610e38d788863264ebbb1373de7c5e1ef32d
Administrator <123@qq.com>
2017-02-22 23:48:03 +0800

    notify and counter

--- src/thread/NotifyTest.java
+++ src/thread/NotifyTest.java
@@ -4,21 +4,26 @@ package thread;
  * Created by Administrator on 2017/2/22.
  */
 public class NotifyTest {
-    private  String flag = "true";
+    private String flag[] = { "true" };
+//    private String flag =  "true" ;
 
-    class NotifyThread extends Thread{
+    class NotifyThread extends Thread {
         public NotifyThread(String name) {
             super(name);
         }
+
         public void run() {
             try {
-                sleep(3000);//推迟3秒钟通知
+                sleep(3000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
-
-            flag = "false";
-            flag.notify();
+            synchronized (flag) {
+                flag[0] = "false";
+//                flag = "false"; //异常 flag重新赋值后其所指的内存地址发生改变 再调用notify方法时，没有获取新引用对象的控制权
+                System.out.println("NotifyThread starting");
+                flag.notify();
+            }
         }
     };
 
@@ -28,27 +33,29 @@ public class NotifyTest {
         }
 
         public void run() {
+            synchronized (flag) {
+                while (flag[0] != "false") {
+//                while (flag != "false") {
+                    System.out.println(getName() + " begin waiting!");
+                    long waitTime = System.currentTimeMillis();
+                    try {
+                        flag.wait();
 
-            while (flag!="false") {
-                System.out.println(getName() + " begin waiting!");
-                long waitTime = System.currentTimeMillis();
-                try {
-                    flag.wait();
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                    waitTime = System.currentTimeMillis() - waitTime;
+                    System.out.println("wait time :" + waitTime);
                 }
-                waitTime = System.currentTimeMillis() - waitTime;
-                System.out.println("wait time :"+waitTime);
+                System.out.println(getName() + " end waiting!");
             }
-            System.out.println(getName() + " end waiting!");
-
         }
     }
 
     public static void main(String[] args) throws InterruptedException {
         System.out.println("Main Thread Run!");
         NotifyTest test = new NotifyTest();
-        NotifyThread notifyThread =test.new NotifyThread("notify01");
+        NotifyThread notifyThread = test.new NotifyThread("notify01");
         WaitThread waitThread01 = test.new WaitThread("waiter01");
         WaitThread waitThread02 = test.new WaitThread("waiter02");
         WaitThread waitThread03 = test.new WaitThread("waiter03");
@@ -57,4 +64,5 @@ public class NotifyTest {
         waitThread02.start();
         waitThread03.start();
     }
+
 }
